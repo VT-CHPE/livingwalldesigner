40b29b1a1727ec6a2433c5d4359a2ec8f14c6b88
Michael Stewart <tgm@vt.edu>
2015-08-06 13:45:32 -0400

    auto-gen yo angular starter

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name livingwalldesignerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the livingwalldesignerApp
+ */
+angular.module('livingwalldesignerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name livingwalldesignerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the livingwalldesignerApp
+ */
+angular.module('livingwalldesignerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
