e5b6d28ede59e89080e42ca0e742c0e5cf0c95c0
Michael Stewart <tgm@vt.edu>
2015-09-11 16:11:39 -0400

    wanted to show interaction with controller and template to nerds

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,8 @@
  * Controller of the livingwalldesignerApp
  */
 angular.module('livingwalldesignerApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
