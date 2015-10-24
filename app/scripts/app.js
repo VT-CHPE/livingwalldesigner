4c58d234c5fa48085e7f0b2a7666d9ec32098388
Philippe Gray <philippe@vt.edu>
2015-10-24 19:15:05 -0400

    Added new route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,17 +22,17 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: '../views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
       .when('/about', {
-        templateUrl: 'views/about.html',
+        templateUrl: '../views/about.html',
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
       .when('/login', {
-		  templateUrl: 'views/login.html',
+		  templateUrl: '../views/login.html',
 		  controller: 'LoginCtrl',
 		  controllerAs: 'login'
 	  })
