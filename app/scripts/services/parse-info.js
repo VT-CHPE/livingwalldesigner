94113028e79ceb90912693a8c0ead3ff1bfc2172
Wanwei Zheng <wwzheng@vt.edu>
2015-10-30 10:06:10 -0400

    add parse-info.js

---
+++ app/scripts/services/parse-info.js
@@ -0,0 +1,5 @@
+angular
+  .module('livingwalldesignerAux', [])
+  .service('ParseInfo', [function(){
+    Parse.initialize("IezLww1VqlVtg2weYoLxc0qGlUX1qsEQrbpnqvyF", "83GVLuERShy6iGGYwQG3Tw0EXASTfLSFUvpQwRBc");
+  }]);
